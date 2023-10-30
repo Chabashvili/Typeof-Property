e40c5e3b5119dfc375334c20fdae1aa15323c0a1
Chabashvili <sandro.chabashvili.88@gmail.com>
2023-10-30 19:58:11 +0400

    Add project files.

---
+++ Typeof Property/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typeof_Property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType = typeof(MyClass);
+            object instance = Activator.CreateInstance(myType);
+
+            PropertyInfo prop1 = myType.GetProperty("MyProperty1");
+            PropertyInfo prop2 = myType.GetProperty("MyProperty2");
+
+            prop1.SetValue(instance, 42);
+            prop2.SetValue(instance, "Hello, Reflection!");
+
+            Console.WriteLine("MyProperty1 Value: " + prop1.GetValue(instance));
+            Console.WriteLine("MyProperty2 Value: " + prop2.GetValue(instance));
+        }
+        public class MyClass
+        {
+            public int MyProperty1 { get; set; }
+            public string MyProperty2 { get; set; }
+        }
+    }
+}
